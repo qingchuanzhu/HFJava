e0a233a6f2fc27b544f2134fd0248c2751dc9bf0
SevenYoona <sy19900513ymsfd@gmail.com>
2018-09-01 13:43:17 -0700

    Check in some classes

---
+++ Cat.java
@@ -0,0 +1,9 @@
+class Cat{
+	int size;
+	String breed;
+	String name;
+
+	void bark(){
+		System.out.println("Miao! Miao!");
+	}
+}
---
+++ DogTestDrive.java
@@ -0,0 +1,7 @@
+public class DogTestDrive{
+	public static void main(String[] args) {
+		Cat d = new Cat();
+		d.size = 40;
+		d.bark();	
+	}
+}
