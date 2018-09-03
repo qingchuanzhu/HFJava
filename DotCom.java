15f4189b7f93ac228d229c884199c3926a6c3f9a
SevenYoona <sy19900513ymsfd@gmail.com>
2018-09-03 14:53:54 -0700

    Finished the DotComBust game

---
+++ DotCom.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+
+public class DotCom {
+	private ArrayList<String> locationCells;
+	private String name;
+
+	public String checkYourself(String stringGuess) {
+		String result = "miss";
+
+		int index = locationCells.indexOf(stringGuess);
+
+		if (index >= 0) {
+			locationCells.remove(index);
+			if (locationCells.isEmpty()) {
+				result = "kill";
+				System.out.println("Ouch! You sunk " + name + "   :  ( ");
+			} else {
+				result = "hit";
+			}
+		}
+
+		return result;
+	}
+
+	public void setLocationCells(ArrayList<String> cellLocations){
+		locationCells = cellLocations;
+	}
+
+	public void setName(String n){
+		name = n;
+	}
+}
--- SimpleDotComGame.java
+++ SimpleDotComGame.java
@@ -1,23 +1,3 @@
-import java.io.*;
-
-class GameHelper {
-	public String getUserInput(String prompt) {
-		String inputLine = null;
-		System.out.print(prompt + " ");
-		try {
-			BufferedReader is = new BufferedReader(new InputStreamReader(System.in));
-			inputLine = is.readLine();
-			if (inputLine.length() == 0) {
-				return null;
-			}
-		} catch (IOException e) {
-			System.out.println("IOException: " + e);
-		}
-
-		return inputLine;
-	}
-}
-
 public class SimpleDotComGame {
 	public static void main(String[] args) {
 		int numOfGuesses = 0;
