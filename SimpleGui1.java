433b9ef4bc054f3af54508387325d0dae906b30b
SevenYoona <sy19900513ymsfd@gmail.com>
2018-09-15 14:10:32 -0700

    Inner class try out

--- SimpleGui1.java
+++ SimpleGui1.java
@@ -2,35 +2,47 @@ import javax.swing.*;
 import java.awt.event.*;
 import java.awt.*;
 
-class SimpleGui1 implements ActionListener {
+class SimpleGui1 {
 
-	JButton button;
 	JFrame frame;
+	JLabel label;
 
-	void changeIt(){
+	class LabelListener implements ActionListener {
+		public void actionPerformed(ActionEvent event) {
+			label.setText("Ouch!");
+		}
+	}
 
+	class ColorListener implements ActionListener {
+		public void actionPerformed(ActionEvent event) {
+			frame.repaint();
+		}
 	}
 
 	void go() {
 		frame = new JFrame();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		button = new JButton("change colors");
-		button.addActionListener(this);
+		JButton colorButton = new JButton("change colors");
+		colorButton.addActionListener(new ColorListener());
+
+		JButton labelButton = new JButton("Change Label");
+		labelButton.addActionListener(new LabelListener());
+
+		label = new JLabel("I'm a Label");
 
 		MyDrawPanel drawPanel = new MyDrawPanel();
-
 		
-		frame.getContentPane().add(BorderLayout.SOUTH, button);
+		frame.getContentPane().add(BorderLayout.SOUTH, colorButton);
 		frame.getContentPane().add(BorderLayout.CENTER, drawPanel);
+		frame.getContentPane().add(BorderLayout.EAST, labelButton);
+		frame.getContentPane().add(BorderLayout.WEST, label);
 
 		frame.setSize(300, 300);
 		frame.setVisible(true);
 	}
 
-	public void actionPerformed(ActionEvent event) {
-		frame.repaint();
-	}
+	
 
 	public static void main(String[] args) {
 		SimpleGui1 gui = new SimpleGui1();
