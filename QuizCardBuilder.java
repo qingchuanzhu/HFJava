04a26d6110acd1acce28fa41ce05ff28151de277
Qingchuan Zhu <zqc@utexas.edu>
2018-09-16 22:29:25 -0700

    added button handler imp and save file imp

--- QuizCardBuilder.java
+++ QuizCardBuilder.java
@@ -13,7 +13,9 @@ class QuizCardBuilder {
 	void go() {
 		// build and display gui
 		frame = new JFrame("Quiz Card Builder");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		JPanel mainPanel = new JPanel();
+		mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
 		Font bigFont = new Font("sanserif", Font.BOLD, 24);
 		question = new JTextArea(6,20);
 		question.setLineWrap(true);
@@ -69,24 +71,50 @@ class QuizCardBuilder {
 	class NextCardListener implements ActionListener {
 		public void actionPerformed(ActionEvent ev) {
 		// add the current card to the list and clear the text areas
+			QuizCard card = new QuizCard(question.getText(), answer.getText());
+			cardList.add(card);
+			clearCard();
 		}
 	}
 
 	class SaveMenuListener implements ActionListener {
 		public void actionPerformed(ActionEvent ev) {
 		// bring up a file dialog box, let the user name and save the set
+			QuizCard card = new QuizCard(question.getText(), answer.getText());
+			cardList.add(card);
+
+			JFileChooser fileSave = new JFileChooser();
+			fileSave.showSaveDialog(frame);
+			saveFile(fileSave.getSelectedFile());
 		}
 	}
 
 	class NewMenuListener implements ActionListener {
 		public void actionPerformed(ActionEvent ev) {
 		// clear out the card list, and clear out the text area
+			cardList.clear();
+			clearCard();
 		}
 	}
 
 	private void saveFile(File file) {
 		// iterate through the list of cards, and write each one out to a text file
+		try {
+			BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+			for (QuizCard card : cardList) {
+				writer.write(card.getQuestion() + "/");
+				writer.write(card.getAnswer() + "\n");
+			}
+			writer.close();
+		} catch(Exception ex) {
+			ex.printStackTrace();
+		}
+	}
 
+	private void clearCard() {
+		question.setText("");
+		answer.setText("");
+		question.requestFocus();
 	}
 
 	public static void main(String[] args) {
