9f852fc8648471ece5f3ad7de91f046bdad1c022
Qingchuan Zhu <zqc@utexas.edu>
2018-09-18 21:07:03 -0700

    Created the Quiz Card Player template

---
+++ QuizCardPlayer.java
@@ -0,0 +1,30 @@
+class QuizCardPlayer {
+	void go() {
+		// build and display GUI
+	}
+
+	class NextCardListener implements ActionListener {
+		public void actionPerformed(ActionEvent ev) {
+
+		}
+	}
+
+	class OpenMenuListener implements ActionListener {
+		public void actionPerformed(ActionEvent ev) {
+			
+		}
+	}
+
+	private void loadFile(File file) {
+
+	}
+
+	private void makeCard(String lineToParse) {
+
+	}
+
+	public static void main(String[] args) {
+		QuizCardPlayer reader = new QuizCardPlayer();
+		reader.go();
+	}
+}
