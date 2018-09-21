bb2639a6664d5d83ebf5509a6f64cdefc6beda44
Qingchuan Zhu <zqc@utexas.edu>
2018-09-20 22:52:45 -0700

    Added DailyAdviceClient

---
+++ DailyAdviceCilent.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.net.*;
+
+class DailyAdviceClient{
+	void go() {
+		try {
+			Socket s = new Socket("127.0.0.1", 4242);
+			InputStreamReader streamReader = new InputStreamReader(s.getInputStream());
+			BufferedReader reader = new BufferedReader(streamReader);
+
+			String advice = reader.readLine();
+			System.out.println("Today you should: " + advice);
+			reader.close();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		DailyAdviceClient client = new DailyAdviceClient();
+		client.go();
+	}
+}
