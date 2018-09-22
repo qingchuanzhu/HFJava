a962b407231cd3f622ddca52739c148b652cf7d7
Qingchuan Zhu <zqc@utexas.edu>
2018-09-22 15:34:34 -0700

    Try out thread names

---
+++ RunThreads.java
@@ -0,0 +1,20 @@
+class RunThreads implements Runnable {
+
+	public static void main(String[] args) {
+		RunThreads job = new RunThreads();
+		Thread alpha = new Thread(job);
+		Thread beta = new Thread(job);
+		alpha.setName("Alpha thread");
+		beta.setName("Beta thread");
+
+		alpha.start();
+		beta.start();
+	}
+
+	public void run() {
+		for (int i = 0; i < 25; i++) {
+			String threadName = Thread.currentThread().getName();
+			System.out.println(threadName + " is running");
+		}
+	}
+}
